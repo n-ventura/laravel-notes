2cb0368b60add9864d6ae4bc8f7ccd3b47120f35
n.ventura <nelsonventur@gmail.com>
2024-09-20 08:49:19 +0100

    execução dos seeder: php artisan db:seed UserTableSeeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserTableSeeder extends Seeder
 {
@@ -12,6 +13,26 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //create multiple users
+        DB::table('users')->insert(
+            [
+                [
+                    'username' => 'user1@gmail.com',
+                    'password' => bcrypt('123456'),
+                    'created_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'username' => 'user2@gmail.com',
+                    'password' => bcrypt('123456'),
+                    'created_at' => date('Y-m-d H:i:s')
+                ],
+                [
+                    'username' => 'user3@gmail.com',
+                    'password' => bcrypt('123456'),
+                    'created_at' => date('Y-m-d H:i:s')
+                ]
+
+
+        ]);
     }
 }
