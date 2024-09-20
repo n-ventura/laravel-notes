fba9f8788a0b1f37ee1d7040ef2a289cbd242a6b
n.ventura <nelsonventur@gmail.com>
2024-09-20 08:57:30 +0100

    php artisan make:model User

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -33,7 +34,13 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        echo 'ok!';
+        //get all tuhe users from the database
+
+        $users = User::all()->toArray();
+
+        echo '<pre>';
+
+        print_r($users);
     }
 
     public function logout()
