7ffc2ac890276689aec73332c51d16724b762cae
n.ventura <nelsonventur@gmail.com>
2024-09-22 13:12:21 +0100

    criação da top bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,26 +7,7 @@
     <div class="row justify-content-center">
         <div class="col">
 
-            <div class="row mb-3 align-items-center">
-                <div class="col">
-                    <a href="{{route('home')}}">
-                        <img src="assets/images/logo.png" alt="Notes logo">
-                    </a>
-                </div>
-                <div class="col text-center">
-                    A simple <span class="text-warning">Laravel</span> project!
-                </div>
-                <div class="col">
-                    <div class="d-flex justify-content-end align-items-center">
-                        <span class="me-3"><i class="fa-solid fa-user-circle fa-lg text-secondary me-3"></i>[username]</span>
-                        <a href="{{route('logout')}}" class="btn btn-outline-secondary px-3">
-                            Logout<i class="fa-solid fa-arrow-right-from-bracket ms-2"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <hr>
+            @include('top_bar')
 
             <!-- no notes available -->
             <div class="row mt-5">
---
+++ resources/views/top_bar.blade.php
@@ -0,0 +1,20 @@
+<div class="row mb-3 align-items-center">
+    <div class="col">
+        <a href="{{route('home')}}">
+            <img src="assets/images/logo.png" alt="Notes logo">
+        </a>
+    </div>
+    <div class="col text-center">
+        A simple <span class="text-warning">Laravel</span> project!
+    </div>
+    <div class="col">
+        <div class="d-flex justify-content-end align-items-center">
+            <span class="me-3"><i class="fa-solid fa-user-circle fa-lg text-secondary me-3"></i>[username]</span>
+            <a href="{{route('logout')}}" class="btn btn-outline-secondary px-3">
+                Logout<i class="fa-solid fa-arrow-right-from-bracket ms-2"></i>
+            </a>
+        </div>
+    </div>
+</div>
+
+<hr>
