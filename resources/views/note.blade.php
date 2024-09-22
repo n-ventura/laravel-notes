168c1505739dd1ceaee7cd09b7207def66dbe873
n.ventura <nelsonventur@gmail.com>
2024-09-22 15:44:28 +0100

    Apresentação da data de updated_at na view

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -5,6 +5,9 @@
                 <div class="col">
                     <h4 class="text-info">{{ $note['title']}}</h4>
                     <small class="text-secondary"><span class="opacity-75 me-2">Created at:</span><strong>{{ date('Y-m-d H:i:s', strtotime($note['created_at'])) }}</strong></small>
+                    @if($note['created_at'] != $note['updated_at'])
+                    <small class="text-secondary ms-5"><span class="opacity-75 me-2">Updated at:</span><strong>{{ date('Y-m-d H:i:s', strtotime($note['updated_at'])) }}</strong></small>
+                    @endif
                 </div>
                 <div class="col text-end">
                     <a href="{{ route('edit', ['id' => Crypt::encrypt($note['id'])]) }}" class="btn btn-outline-secondary btn-sm mx-1"><i class="fa-regular fa-pen-to-square"></i></a>
